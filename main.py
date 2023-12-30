058fd055e02578d5638df16e7c04f67ffd6a3b9c
DJuuDi <152791461+DJuuDi@users.noreply.github.com>
2023-12-30 16:27:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLay
 
 app = QApplication([]) #сторюємо віконний додаток
 
-from window import main_line
+from window import *
 
 win = QWidget() # створємо вікно
 win.resize(600, 300)
 win.setWindowTitle("Memori card")
 win.setLayout(main_line)
+
+def answer_click():
+    if answer_btn.text() == "Відповісти":
+        group_box.hide()
+        result_box.show()
+        answer_btn.setText("Наступне питання")
+    else:
+        group_box.show()
+        result_box.hide()
+        answer_btn.setText("Відповісти")
+
+answer_btn.clicked.connect(answer_click)
 
 # в кінці
 win.show() #показує вікно
--- window.py
+++ window.py
@@ -23,13 +23,42 @@ btn3 = QRadioButton("варіант3")
 btn4 = QRadioButton("варіант4")
 
 row2 = QHBoxLayout()
-radio_group = QButtonGroup
+radio_group = QButtonGroup()
 radio_group.addButton(btn1)
 radio_group.addButton(btn2)
 radio_group.addButton(btn3)
 radio_group.addButton(btn4)
 
+group_box = QGroupBox("Відповідь")
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
 
+col1.addWidget(btn1)
+col1.addWidget(btn2)
+col2.addWidget(btn3)
+col2.addWidget(btn4)
+
+row2.addLayout(col1)
+row2.addLayout(col2)
+
+group_box.setLayout(row2)
+
+result_box = QGroupBox("Результат")
+result_text = QLabel("Правильно")
+right_answer_text = QLabel("відповідь")
+
+result_line = QVBoxLayout()
+result_line.addWidget(result_text)
+result_line.addWidget(right_text, alignment=Qt.Alignment, stretch=2)
+
+result_box.setLayout(result_line)
+result_box.hide()
+
+answer_btn = QVBoxLayout()
 
 main_line = QVBoxLayout()
-main_line.addLayout(row1)
+main_line.addLayout(row1, stretch = 1)
+main_line.addWidget(question_lb, stretch = 2, aliqnmant=Qt.AlignCentre)
+main_line.addWidget(group_box, stretch = 6)
+main_line.addWidget(result_box, stretch = 6)
+main_line.addWidget(answer_btn, stretch = 3)
